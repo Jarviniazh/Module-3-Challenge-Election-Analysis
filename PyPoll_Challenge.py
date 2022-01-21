f2f5c43ec25b4c70a3fa3a1827da110d26f63a60
Jarviniazh <zhaojwdkj@gmail.com>
2022-01-20 20:56:21 -0500

    Change the file_to_save name to election_results

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -8,7 +8,7 @@ import os
 # Add a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Add a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_results.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
@@ -100,7 +100,7 @@ with open(file_to_save, "w") as txt_file:
         county_turnout = float(votes_in_county) / float(total_votes) * 100
 
          # 6d: Print the county results to the terminal.
-        county_results = (f"{county_name}: {county_turnout:.1f}% ({votes_in_county:,})")
+        county_results = (f"{county_name}: {county_turnout:.1f}% ({votes_in_county:,})\n")
         print(county_results) 
 
          # 6e: Save the county votes to a text file.
